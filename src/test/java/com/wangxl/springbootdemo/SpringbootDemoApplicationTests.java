957f35a62030dbe48c198400d4bc9ed6724552d0
WangXueLei <15110096722@163.com>
2021-11-10 23:58:50 +0800

    first commit

---
+++ src/test/java/com/wangxl/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangxl.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
